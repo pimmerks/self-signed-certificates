de22659208c5e5a4c2b8bcc34cacdb49939aef70
Pim Merks <merks.pim@gmail.com>
2021-06-29 15:26:47 +0200

    Updated original code with better configurability, logging and password protected key

--- create-ca.sh
+++ create-ca.sh
@@ -1,32 +1,54 @@
 #!/usr/bin/env bash
-
-# Generates your own Certificate Authority for development.
-# This script should be executed just once.
-
 set -e
 
-if [ -f "ca.crt" ] || [ -f "ca.key" ]; then
-    echo -e "\e[41mCertificate Authority files already exist!\e[49m"
+# Get environment variables from the .env file
+set -o allexport; source .env; set +o allexport
+
+# Logging
+function log_ok()   { echo -e "\e[32m✔ $1\e[39m"; }
+function log_err()  { echo -e "\e[31m✗ $1\e[39m"; }
+function log_info() { echo -e "\e[39m$1\e[39m"; }
+function log_cmd_output { echo -n -e "\e[90m"; }
+
+
+if [ -f "$CA_CRT" ] || [ -f "$CA_KEY" ]; then
+    log_err "Certificate Authority files already exist!"
     echo
-    echo -e "You only need a single CA even if you need to create multiple certificates."
-    echo -e "This way, you only ever have to import the certificate in your browser once."
+    log_info "You only need a single CA even if you need to create multiple certificates."
+    log_info "This way, you only ever have to import the certificate in your browser once."
     echo
-    echo -e "If you want to restart from scratch, delete the \e[93mca.crt\e[39m and \e[93mca.key\e[39m files."
-    exit
+    log_info "If you want to restart from scratch, delete the \e[93m$CA_CRT\e[39m and \e[93m$CA_KEY\e[39m files."
+    exit 1
 fi
 
+log_info "Specify a new password for your CA key, please keep this somewhere safe!"
+echo -n "Password: "
+read -s CA_PASSWORD
+echo
+log_cmd_output
+
+
 # Generate private key
-openssl genrsa -out ca.key 2048
+openssl genrsa -passout pass:$CA_PASSWORD -out $CA_KEY -aes256 $CA_KEY_BITS
+log_ok "Created CA key file completed"
+
 
 # Generate root certificate
-openssl req -x509 -new -nodes -subj "/C=US/O=_Development CA/CN=Development certificates" -key ca.key -sha256 -days 3650 -out ca.crt
+openssl req -x509 -new \
+  -subj "/C=$SUBJ_C/ST=$SUBJ_ST/L=$SUBJ_L/O=$SUBJ_O/OU=$SUBJ_OU/CN=$SUBJ_CN/emailAddress=$SUBJ_EMAIL" \
+  -key $CA_KEY \
+  -sha256 \
+  -passin pass:$CA_PASSWORD \
+  -days $CA_CRT_DAYS \
+  -out $CA_CRT
 
-echo -e "\e[42mSuccess!\e[49m"
+
+log_ok "Creating root certificate completed"
 echo
-echo "The following files have been written:"
-echo -e "  - \e[93mca.crt\e[39m is the public certificate that should be imported in your browser"
-echo -e "  - \e[93mca.key\e[39m is the private key that will be used by \e[93mcreate-certificate.sh\e[39m"
+log_info "The following files have been written:"
+log_info "  - \e[93m$CA_CRT\e[39m is the public certificate that should be imported in your browser"
+log_info "  - \e[93m$CA_KEY\e[39m is the private key that will be used by \e[93mcreate-certificate.sh\e[39m"
 echo
-echo "Next steps:"
-echo -e "  - Import \e[93mca.crt\e[39m in your browser"
-echo -e "  - run \e[93mcreate-certificate.sh example.com\e[39m"
+log_info "Next steps:"
+log_info "  - Import \e[93m$CA_CRT\e[39m in your browser"
+log_info "  - run \e[93mcreate-certificate.sh example.com\e[39m"
--- create-certificate.sh
+++ create-certificate.sh
@@ -4,32 +4,46 @@
 
 set -e
 
+# Get environment variables from the .env file
+set -o allexport; source .env; set +o allexport
+
+# Logging
+function log_ok()   { echo -e "\e[32m✔ $1\e[39m"; }
+function log_err()  { echo -e "\e[31m✗ $1\e[39m"; }
+function log_info() { echo -e "\e[39m$1\e[39m"; }
+function log_cmd_output { echo -n -e "\e[90m"; }
+
 if [ -z "$1" ]; then
-    echo -e "\e[43mMissing domain name!\e[49m"
+    log_err "\e[43mMissing domain name!\e[49m"
     echo
-    echo "Usage: $0 example.com"
+    log_info "Usage: $0 example.com"
     echo
-    echo "This will generate a wildcard certificate for the given domain name and its subdomains."
-    exit
+    log_info "This will generate a wildcard certificate for the given domain name and its subdomains."
+    exit 1
 fi
 
 DOMAIN=$1
 
-if [ ! -f "ca.key" ]; then
-    echo -e "\e[41mCertificate Authority private key does not exist!\e[49m"
+if [ ! -f "$CA_KEY" ]; then
+    log_err "Certificate Authority private key does not exist!"
     echo
-    echo -e "Please run \e[93mcreate-ca.sh\e[39m first."
-    exit
+    log_info "Please run \e[93mcreate-ca.sh\e[39m first."
+    exit 1
 fi
 
+mkdir $DOMAIN
+log_ok "Creating directory $DOMAIN complete"
+
 # Generate a private key
-openssl genrsa -out "$DOMAIN.key" 2048
+openssl genrsa -out "$DOMAIN/$DOMAIN.key" $DOMAIN_KEY_BITS
+log_ok "Creating private key complete ($DOMAIN/$DOMAIN.key)"
 
 # Create a certificate signing request
-openssl req -new -subj "/C=US/O=Local Development/CN=$DOMAIN" -key "$DOMAIN.key" -out "$DOMAIN.csr"
+openssl req -new -subj "/C=$SUBJ_C/ST=$SUBJ_ST/L=$SUBJ_L/O=$SUBJ_O/OU=$SUBJ_OU/emailAddress=$SUBJ_EMAIL/CN=$DOMAIN" -key "$DOMAIN/$DOMAIN.key" -out "$DOMAIN/$DOMAIN.csr"
+log_ok "Creating certificate signing request complete ($DOMAIN/$DOMAIN.csr)"
 
 # Create a config file for the extensions
->"$DOMAIN.ext" cat <<-EOF
+>"$DOMAIN/$DOMAIN.ext" cat <<-EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -40,21 +54,20 @@ DNS.1 = $DOMAIN
 DNS.2 = *.$DOMAIN
 EOF
 
+log_ok "Creating config file complete ($DOMAIN/$DOMAIN.ext)"
+
 # Create the signed certificate
 openssl x509 -req \
-    -in "$DOMAIN.csr" \
-    -extfile "$DOMAIN.ext" \
-    -CA ca.crt \
-    -CAkey ca.key \
+    -in "$DOMAIN/$DOMAIN.csr" \
+    -extfile "$DOMAIN/$DOMAIN.ext" \
+    -CA $CA_CRT \
+    -CAkey $CA_KEY \
     -CAcreateserial \
-    -out "$DOMAIN.crt" \
+    -out "$DOMAIN/$DOMAIN.crt" \
     -days 365 \
     -sha256
 
-rm "$DOMAIN.csr"
-rm "$DOMAIN.ext"
-
 echo -e "\e[42mSuccess!\e[49m"
 echo
-echo -e "You can now use \e[93m$DOMAIN.key\e[39m and \e[93m$DOMAIN.crt\e[39m in your web server."
+echo -e "You can now use \e[93m$DOMAIN/$DOMAIN.key\e[39m and \e[93m$DOMAIN/$DOMAIN.crt\e[39m in your web server."
 echo -e "Don't forget that \e[1myou must have imported \e[93mca.crt\e[39m in your browser\e[0m to make it accept the certificate."
